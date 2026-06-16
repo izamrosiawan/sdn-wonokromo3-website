99e82a8ed3d0237d73471658bfd4d8f7b208d0aa
yggdrasil <izamrosiawan13@gmail.com>
2026-06-16 13:32:56 +0700

    apdet

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 import Beranda from "./pages/Beranda";
 import Profil from "./pages/Profil";
